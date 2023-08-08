ea3072cb5a394b88daa9b873ec7b44a2741de2e3
Raji Johnson <rjohnson1906@gmail.com>
2023-08-08 15:35:38 -0400

    Updated Push List on Screen

--- src/components/app.js
+++ src/components/app.js
@@ -1,25 +1,67 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ToDoList from './ToDoList';
-import TaskDetails from './TaskDetails';
+import React, { useState } from 'react';
+import '../style/app.scss';
 
+export default function App() {
+  const [toDoListArray, setToDoListArray] = useState([]);
+  const [inputValue, setInputValue] = useState('');
 
-export default class App extends Component {
-  render() {
-    return (
-      <div className='app'>
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const itemId = String(Date.now());
+    const toDoItem = inputValue;
+    addItemToArray(itemId, toDoItem);
+    setInputValue('');
+  };
 
-        <Router>
+  const handleItemClick = (id) => {
+    removeItemFromArray(id);
+  };
+
+  const addItemToArray = (itemId, toDoItem) => {
+    setToDoListArray([...toDoListArray, { itemId, toDoItem }]);
+    console.log(toDoListArray);
+  };
+
+  const removeItemFromArray = (id) => {
+    setToDoListArray((prevArray) => prevArray.filter((item) => item.itemId !== id));
+    console.log(toDoListArray);
+  };
+
+  return (
+    <div>
+      <h1>Done Is Better Than "Perfect"</h1>
+      <section className="container">
+        <div className="heading">
+          <img className="heading__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/756881/laptop.svg" alt="Laptop" />
+          <h1 className="heading__title">To-Do</h1>
+        </div>
+        <form className="form" onSubmit={handleFormSubmit}>
           <div>
-            <h1>To Do List</h1>
-            <Switch>
-              <Route exact path="/" component={ToDoList} />
-              <Route path="/details/:taskId" component={TaskDetails} />
-            </Switch>
+            <label className="form__label" htmlFor="todo">~ Things To Make Happen ~</label>
+            <input
+              className="form__input"
+              type="text"
+              id="todo"
+              name="to-do"
+              size="30"
+              autoComplete="off"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              required
+            />
+            <button className="button"><span>Submit</span></button>
           </div>
-        </Router>
-
-      </div>
-    );
-  }
+        </form>
+        <div>
+          <ul className="toDoList">
+            {toDoListArray.map((item) => (
+              <li key={item.itemId} data-id={item.itemId} onClick={() => handleItemClick(item.itemId)}>
+                {item.toDoItem}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
 }
